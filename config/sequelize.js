ec53d91ff319b9ecb390135be82ee043ae79f27b
RealizeUser <157451793+RealizeUser@users.noreply.github.com>
2024-09-25 15:54:56 +0530

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -6,9 +6,19 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: "mysql" || process.env.DB_DRIVER,
     logging: false,
+    dialectOptions: process.env.NODE_ENV == 'development' ? {} : {
+      ssl: {
+        require: true, // This ensures SSL is used
+        rejectUnauthorized: false, // This is for self-signed certificates, set to true for production
+        // You can provide additional options here, such as:
+        // ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+        // key: fs.readFileSync('/path/to/client-key/client-key.key').toString(),
+        // cert: fs.readFileSync('/path/to/client-certificates/client-cert.crt').toString(),
+      },
+    },
     // pool: {
     //   max: 100,
     //   min: 0,
